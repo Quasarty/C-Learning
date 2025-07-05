6e4a3606c537eb952c15143a6787f05c4c542d83
Quasarty <andrey.kozhevnikov.240506@gmail.com>
2025-07-05 14:41:02 +0900

    Add files via upload

--- Modules/files.c
+++ Modules/files.c
@@ -5,12 +5,14 @@
 #include <stdlib.h>
 #include <stddef.h>
 #include <stdbool.h>
-#include <ctype.h>
-#include <time.h>
+#include <string.h>
+#include <wchar.h>
 
 const int range = 43; //< Количество чисел в границах рандома
 const int range_shift = 48; //< Сдвиг в таблице символов
 const int num_range = 10; //< Количество чисел в границах рандома для гарантированного числа
+const int max_str_length = 256; //< Максимальная длина строки в файле учеников
+
 
 /**
  * @brief Генерирует текстовый файл из случайных символов
@@ -51,7 +53,7 @@ void generate_file(char* file_name, size_t char_count, bool guarantee){
  */
 char* get_two_chars(char* file_name){
     char* chars = calloc(2, sizeof(char)); //< Возвращаемые символы
-    //Открытие файал
+    //Открытие файла
     FILE* file_var = fopen(file_name, "r");
     if (file_var == NULL)
         return NULL;
@@ -65,4 +67,86 @@ char* get_two_chars(char* file_name){
         
     return chars;
         
+}
+
+
+/**
+ * @brief Процедура сравнивает две строки, содержащих информацию об учениках
+ * 
+ * @param student_a - строка, содержащая информацию о первом ученике
+ * @param student_b - строка, содержащая информацию о втором ученике
+ * @return true: если имя и фамилия двух учеников совпадает;
+ * @return false: если имя или фамилия двух учеников не совпадает
+ */
+bool compare_student(wchar_t* student_a, wchar_t* student_b){
+    //Переменные для имени и фамилии сравниваемых учеников
+    wchar_t name_a[64], surname_a[64], name_b[64], surname_b[64]; 
+
+    //Считываем имя и фамилию
+    swscanf(student_a, L"%ls %ls", name_a, surname_a);
+    swscanf(student_b, L"%ls %ls", name_b, surname_b);
+
+    //Сравниваем
+    if (wcscmp(name_a, name_b) == 0 && wcscmp(surname_a, surname_b) == 0)
+        return true;
+    else
+        return false;
+}
+
+
+/**
+ * @brief Процедура сравнивает всех учеников в файле
+ * 
+ * @param file_name - имя файла
+ * @return true: если есть два ученика с одинаковым именем и фамилией
+ * @return false: если нету двух учеников с одинаковым именем и фамилией
+ */
+bool file_compare_students(char* file_name){
+    //Открытие файла
+    FILE* file_var = fopen(file_name, "r");
+    if (file_var == NULL)
+        return false;
+
+    wchar_t str[max_str_length]; //< Текущая рассматриваемая строка 
+    wchar_t compare_str[max_str_length]; //< Текущая строка для сравнения
+
+    //Проверяем строки файла
+    while (!feof(file_var)){
+        //Открываем файл еще раз, но в другую переменную
+        FILE* compare_file_var = fopen(file_name, "r");
+        if (compare_file_var == NULL){
+            fclose(file_var);
+            return false;
+        }
+        
+        //Считываем строку
+        fgetws(str, max_str_length, file_var);
+        //Достигли конца файла, сравнивать не с чем
+        if (feof(file_var)){
+            fclose(file_var);
+            fclose(compare_file_var);
+            return false;
+        }
+
+        //Перемещаем курсор во второй переменной на одну строку вперед
+        fseek(compare_file_var, ftell(file_var), SEEK_SET);
+
+        //Сравниваем каждую последующую строку с текущей
+        while(!feof(compare_file_var)){
+            //Считываем строку для сравнения
+            fgetws(compare_str, max_str_length, compare_file_var);
+
+            //Сравниваем строки
+            if (compare_student(str, compare_str)){
+                fclose(file_var);
+                fclose(compare_file_var);
+                return true;
+            }
+        }
+
+        fclose(compare_file_var);
+    }    
+    fclose(file_var);
+    return false;
+    
 }
--- Modules/files.h
+++ Modules/files.h
@@ -5,14 +5,15 @@
 #include <stdlib.h>
 #include <stddef.h>
 #include <stdbool.h>
-#include <ctype.h>
-#include <time.h>
+#include <string.h>
+#include <wchar.h>
 
 #pragma once
 
 extern const int range; //< Количество чисел в границах рандома
 extern const int range_shift; //< Сдвиг в таблице символов
 extern const int num_range; //< Количество чисел в границах рандома для гарантированного числа
+extern const int max_str_length; //< Максимальная длина строки в файле учеников
 
 /**
  * @brief Генерирует текстовый файл из случайных символов
@@ -32,3 +33,24 @@ void generate_file(char* file_name, size_t char_count, bool guarantee);
  * @return char* - массив из двух символов
  */
 char* get_two_chars(char* file_name);
+
+
+/**
+ * @brief Процедура сравнивает две строки, содержащих информацию об учениках
+ * 
+ * @param student_a - строка, содержащая информацию о первом ученике
+ * @param student_b - строка, содержащая информацию о втором ученике
+ * @return true: если имя и фамилия двух учеников совпадает;
+ * @return false: если имя или фамилия двух учеников не совпадает
+ */
+bool compare_student(wchar_t* student_a, wchar_t* student_b);
+
+
+/**
+ * @brief Процедура сравнивает всех учеников в файле
+ * 
+ * @param file_name - имя файла
+ * @return true: если есть два ученика с одинаковым именем и фамилией
+ * @return false: если нету двух учеников с одинаковым именем и фамилией
+ */
+bool file_compare_students(char* file_name);
